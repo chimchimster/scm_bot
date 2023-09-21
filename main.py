267128b31ee1c009a34468dbd03eb934cc3fbc5a
chimchimster <jorddyishere@gmail.com>
2023-09-21 10:39:21 +0600

    started bot

--- main.py
+++ main.py
@@ -4,19 +4,25 @@ import asyncio
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
-from aiogram.types import Message
-
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
+from aiogram.utils.keyboard import InlineKeyboardBuilder, InlineKeyboardButton, InlineKeyboardMarkup
 
 load_dotenv()
 token = os.environ.get('TOKEN')
 dp = Dispatcher()
 
+builder = InlineKeyboardBuilder()
+
+for index in range(1, 11):
+    builder.button(text=f"Set {index}", callback_data=f"set:{index}")
+
+builder.adjust(3, 2)
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
 
-    await message.answer('Макс привет!')
+    await message.answer('hello from bot!', reply_markup=builder.as_markup())
 
 
 async def main():
