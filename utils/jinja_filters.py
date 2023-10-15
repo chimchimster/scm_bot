aee47880c227c5fb892c4b5535698fcc5ba0eb39
chimchimster <jorddyishere@gmail.com>
2023-10-15 18:37:03 +0600

    change in order_detail.html

---
+++ utils/jinja_filters.py
@@ -0,0 +1,3 @@
+async def round_float_value(value: float):
+
+    return round(value, 2)
--- utils/jinja_template.py
+++ utils/jinja_template.py
@@ -1,17 +1,22 @@
 import pathlib
+
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
+from .jinja_filters import round_float_value
 
 template_env = Environment(loader=FileSystemLoader(
         pathlib.Path.cwd() / 'templates'
     ),
-    autoescape=select_autoescape(['html'])
+    autoescape=select_autoescape(['html']),
+    enable_async=True,
 )
+
+template_env.filters['round_float_value'] = round_float_value
 
 
 async def render_template(template_name: str, **context) -> str:
 
     template = template_env.get_template(template_name)
-    html = template.render(context)
+    html = await template.render_async(context)
 
     return html
